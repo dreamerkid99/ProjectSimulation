39dd329e665fd70c1f9e0a4d937dab938c6177f1
resthiana2 <resthiana123@gmail.com>
2020-12-06 09:41:40 +0700

    added Tim class

---
+++ src/main/java/org/ifunpas/kpl/kode/Tim.java
@@ -0,0 +1,33 @@
+package org.ifunpas.kpl.kode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tim {
+	private String nama;
+	private List<Pemain> pemainInti = new ArrayList<Pemain>();
+	
+	public Tim() {
+		nama = "";
+	}
+	
+	public Tim(String nama) {
+		this.nama = nama;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public List<Pemain> getPemainInti() {
+		return pemainInti;
+	}
+
+	public void setPemainInti(List<Pemain> pemainInti) {
+		this.pemainInti = pemainInti;
+	}
+}
